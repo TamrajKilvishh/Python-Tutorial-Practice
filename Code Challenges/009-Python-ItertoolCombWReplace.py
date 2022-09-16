135ec82b834af216663c30a73442023dea875708
SheshNaagam <astitvasinha1221@protonmail.com>
2022-09-16 21:23:31 +0530

    Practice

--- Code Challenges/009-Python-ItertoolCombWReplace.py
+++
@@ -1,7 +0,0 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-from itertools import combinations_with_replacement
-
-alpha, gamma = [int(base) if base.isdigit() else base for base in input().split()]
-
-for i in combinations_with_replacement(sorted(alpha), gamma):
-    print("".join(i))
--- Code Challenges/010-Python-CompressString.py
+++
@@ -1,8 +0,0 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-from itertools import groupby
-
-stringS = input()
-
-grouping = groupby(stringS)
-for i, j in grouping:
-    print(tuple:=(len(list(j)), int(i)), end=" ")
